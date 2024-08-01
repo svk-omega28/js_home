b9142b45dbd97cb20089c2e5a0e0372260596f3e
svk-omega28 <fallenangel281196@icloud.com>
2024-08-01 11:05:46 +0200

    upload hw2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+
+/* Task #1 Start */
+let admin, name;
+name = "John";
+admin = name;
+alert( admin );
+/* Task #1 End */
+
+
+/* Task #2 Start */
+let planetName = "Mars";
+let currentUserName = "John";
+/* Task #2 End */
+
+/* Task #3 Start */
+let one = "1";
+let two = "2";
+let three = "3";
+let four = "4";
+let five = "5";
+let six = "6";
+let seven = "7";
+let eight = "8";
+let nine = "9";
+let ten = "10";
+
+
+console.log(ten)
+
+/* Task #3 End */
