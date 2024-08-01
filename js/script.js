8ef08bb3d8920aa377a4cc6b38b2310975ab819b
svk-omega28 <fallenangel281196@icloud.com>
2024-08-01 12:22:05 +0200

    upload hw2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+
+const number1 = parseFloat(prompt('Первое число: '));
+const number2 = parseFloat(prompt('Второе число: '));
+const operator = prompt('Выбери действие ( +, -, * ,/ ): ');
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+
+console.log(`${number1} ${operator} ${number2} = ${result}`);
