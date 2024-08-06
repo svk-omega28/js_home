90b5391a8db66ad8a42ed30452d63193b0de4c85
svk-omega28 <fallenangel281196@icloud.com>
2024-08-06 22:44:47 +0200

    upload hw6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// user name
+const userName = prompt("Введите свое имя :");
+
+// number
+const number1 = parseFloat(prompt("Введите первое число:"));
+const number2 = parseFloat(prompt("Введите второе число:"));
+const number3 = parseFloat(prompt("Введите третье число:"));
+
+let sumNumber = (number1 + number2 + number3) / 3;
+
+
+alert( "Привет " + userName + " ,среднее арифметическое трех чисел: " + sumNumber);
