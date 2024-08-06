cdec94f5be9887f21f930e9570670a1fb9273999
svk-omega28 <fallenangel281196@icloud.com>
2024-08-06 20:59:46 +0200

    upload hw5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const userName = prompt("Как вас зовут?");
+console.log(userName);
+
+const hoursString = prompt("Укажите сколько часов вам перевести в секунды?");
+console.log(hoursString);
+
+let hours = Number(hoursString);
+console.log(hours + " часов");
+
+let resultsminutes = Number(hours) * 60;
+console.log(resultsminutes + " минут");
+
+let resultsseconds = Number(resultsminutes) * 60;
+console.log(resultsseconds + " секунд");
+
+alert(`${userName}, результат вашего запроса:\n${"=========================="}\n${hours + " часов"}\n${resultsminutes + " минут (ы)"}\n${resultsseconds + " секунд (ы)"}\n${"=========================="}\n`);
