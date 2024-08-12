c745bbebee593a6b5b019441667a5be52469ed8e
svk-omega28 <fallenangel281196@icloud.com>
2024-08-12 11:52:39 +0200

    upload hm 11

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+
+const numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували' );
+        console.log(typeof numOrStr);
+        break;
+
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        console.log(typeof numOrStr);
+        break;
+
+    case isNaN(+numOrStr):
+        console.log('number is isNaN');
+        console.log(typeof numOrStr);
+        break;
+
+    default:
+        console.log('OK!');
+        console.log(typeof numOrStr);
+        break;
+}
