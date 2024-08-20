a6aed4fb09c84e7dbcf56a28cc3ff77b802a1c79
svk-omega28 <fallenangel281196@icloud.com>
2024-08-20 20:13:48 +0200

    js hm 13

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function generateKey(lenght, characters) {
+  let key = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < lenght; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    key += characters[randomIndex];
+  }
+
+  return key;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
