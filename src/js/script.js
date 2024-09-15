3b893ed08a37c1d86ee8cfcd5286fede94440356
svk-omega28 <fallenangel281196@icloud.com>
2024-09-15 22:22:57 +0200

    update hw24.1

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 
 const constNumber2 = 666;
@@ -15,9 +16,9 @@ const demoVariableScopes = () => {
   }
 
   console.log('цикл закончился');
-  /*  console.log(`let вне цикла ${i}`); // error
+  /!*  console.log(`let вне цикла ${i}`); // error
   console.log(`var вне цикла: ${varNumber}`); // error
-  console.log(`var вне цикла: ${constNumber2}`); // error */
+  console.log(`var вне цикла: ${constNumber2}`); // error *!/
 
   console.log(`var - можно достать его за пределами цикла for: ${varNumber}`);
 };
@@ -31,3 +32,4 @@ demoVariableScopes();
 // varNumber - hoisting поднятие переменных
 
 // более подробно читал вот тут https://doka.guide/js/closures/
+*/
