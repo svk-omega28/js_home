eb38ca1c7dc8935b995b4a7795cd178315a19685
svk-omega28 <fallenangel281196@icloud.com>
2024-09-19 18:53:15 +0200

    update hw29

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13],
+];
+
+const flattenArray = (arr, ...rest) => {
+  if (rest.length > 0) {
+    throw new Error('Function accepts only 1 argument, too much arguments provided');
+  }
+
+  const flatten = (array) => array.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      acc.push(...flatten(item));
+    } else {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
+
+  return flatten(arr);
+};
+
+console.log(flattenArray(complexArray)); // good
+console.log(flattenArray([1, 2, [3, 4]], [5, 6])); // var
