21ab0dc055c877e2c076da9bd998ac9087799465
svk-omega28 <fallenangel281196@icloud.com>
2024-09-19 20:53:28 +0200

    update hw26

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const insertWordInto = (str) => {
+  const parts = str.split(' ');
+
+  return (word) => {
+    const wordWithSpaces = word;
+
+    const insertPosition = Math.floor(Math.random() * (parts.length + 0.5));
+
+    parts.splice(insertPosition, 0, wordWithSpaces);
+
+    return parts.join(' ');
+  };
+};
+
+const insert = insertWordInto('hello world');
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
