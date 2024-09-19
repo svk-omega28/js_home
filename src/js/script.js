da78d4177bfea27ed4a8daccbf68f5904f5f35ec
svk-omega28 <fallenangel281196@icloud.com>
2024-09-19 19:40:04 +0200

    update hw27

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+Function.prototype.myBind = function (context, ...args) {
+  const func = this;
+
+  return function (...newArgs) {
+    context.fn = func;
+    const result = context.fn(...args, ...newArgs);
+    delete context.fn;
+    return result;
+  };
+};
+
+function greet(greeting, punctuation) {
+  return `${greeting}, ${this.name} ${punctuation}`;
+}
+
+const person = { name: prompt('введите ваше имя') };
+
+const greetPerson = greet.myBind(person, prompt('введите ваше приветсвие'));
+
+const punctuationChoice = prompt('Выберите число от 1 до 3 *__*:\nСмайл 1\nСмайл 2\nСмайл 3');
+
+let punctuation;
+if (punctuationChoice === '1') {
+  punctuation = '╭[☉﹏☉]╮';
+} else if (punctuationChoice === '2') {
+  punctuation = '(^˵◕ω◕˵^)';
+} else if (punctuationChoice === '3') {
+  punctuation = '( -_・)';
+} else {
+  punctuation = '???... эх ты а я говорил ввести от 1 до 3';
+}
+
+console.log(greetPerson(punctuation));
