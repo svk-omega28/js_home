cf4ada0523a05714680ff6641c0a62aa2bb4eef9
svk-omega28 <fallenangel281196@icloud.com>
2024-09-23 19:05:27 +0200

    update hw26

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const sum = () => {
+  let total = 0; // начальное число
+  return (num) => total += num;
+};
+
+const add = sum();
+
+console.log(add(3));
+console.log(add(5));
+console.log(add(20));
