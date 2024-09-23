f361cc952a156592d284478f6c0a92fd71fe8051
svk-omega28 <fallenangel281196@icloud.com>
2024-09-23 19:25:35 +0200

    update hw30

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this; // повертаємо об'єкт для ланцюжка
+  },
+  down() {
+    this.step--;
+    return this; // повертаємо об'єкт для ланцюжка
+  },
+  showStep() {
+    alert(this.step);
+    return this; // повертаємо об'єкт для ланцюжка
+  },
+};
+
+ladder
+  .up()
+  .up()
+  .down()
+  .showStep() // 1
+  .down()
+  .showStep(); // 0
