dd2b2e82f95042167df9013f4972b8dd6be342a3
svk-omega28 <fallenangel281196@icloud.com>
2024-09-30 17:44:46 +0200

    update hw34

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const textInput = document.querySelector('#textInput');
+const infoDiv = document.querySelector('.new-div');
+
+textInput.addEventListener('focus', () => {
+  infoDiv.style.display = 'block';
+});
+
+textInput.addEventListener('blur', () => {
+  infoDiv.style.display = 'none';
+});
