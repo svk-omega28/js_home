0fd0ed1de7066637fc95e7e4fc45eef2e733c3f5
svk-omega28 <fallenangel281196@icloud.com>
2024-10-05 15:14:02 +0200

    update hw32 add style.css remove timer

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,42 @@
 'use strict';
 
-const textInput = document.querySelector('#textInput');
-const infoDiv = document.querySelector('.new-div');
+const createHintBox = (message) => {
+  const div = document.createElement('div');
+  div.classList.add('hint-box');
+  div.innerText = message;
+  return div;
+};
 
-textInput.addEventListener('focus', () => {
-  infoDiv.style.display = 'block';
-});
+const addHintBoxToDOM = (hintBox) => {
+  document.body.appendChild(hintBox);
+};
 
-textInput.addEventListener('blur', () => {
-  infoDiv.style.display = 'none';
-});
+const removeHintBoxFromDOM = (hintBox) => {
+  document.body.removeChild(hintBox);
+};
+
+const attachEventsToInput = (inputId, hintMessage) => {
+  const input = document.getElementById(inputId);
+  let hintBox = null;
+
+  input.addEventListener('focus', () => {
+    if (!hintBox) {
+      hintBox = createHintBox(hintMessage);
+      addHintBoxToDOM(hintBox);
+    }
+
+    positionHintBox(input, hintBox);
+    hintBox.style.display = 'block';
+  });
+
+  input.addEventListener('blur', () => {
+    if (hintBox) {
+      removeHintBoxFromDOM(hintBox);
+      hintBox = null;
+    }
+  });
+};
+
+attachEventsToInput('input1', 'test 1');
+attachEventsToInput('input2', 'test 2');
+attachEventsToInput('input3', 'test 3');
