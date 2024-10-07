681b2f0277ab97e8928cf40a40e29be6990a810f
svk-omega28 <fallenangel281196@icloud.com>
2024-10-07 16:34:43 +0200

    update hw36

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const generateListItem = (item) => {
+  const li = document.createElement('li');
+  if (Array.isArray(item)) {
+    li.appendChild(generateList(item));
+  } else {
+    li.textContent = item;
+  }
+  return li;
+};
+
+const generateList = (array) => {
+  const ul = document.createElement('ul');
+  array.forEach((item) => {
+    ul.appendChild(generateListItem(item));
+  });
+  return ul;
+};
+
+const data = [1, 2, [1.1, 'secondary', 1.2, 1.3], 3, 'test', 'primary'];
+console.log(data);
+document.body.appendChild(generateList(data));
